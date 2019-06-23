30adf7801867d6186adc3e3fcc1aad4f8e96475a
Anurag Dulapalli <pdulapalli@gmail.com>
2019-06-22 22:38:05 -0400

    Add logic for computing file checksum and appending to a JSON object

--- files_checksum_json.py
+++ files_checksum_json.py
@@ -7,12 +7,29 @@ import sys
 import re
 import path_validation
 
-stdin_contents = ""
-for line in sys.stdin:
-    stdin_contents += line
+def md5sum(filename):
+    hash = hashlib.md5()
+    with open(filename, "rb") as f:
+        for chunk in iter(lambda: f.read(128 * hash.block_size), b""):
+            hash.update(chunk)
+    return hash.hexdigest()
 
-whitespace_regex_pattern = re.compile(r'\s+')
-files_list = [filePath for filePath in re.sub(whitespace_regex_pattern, '', stdin_contents).split(',') if path_validation.is_pathname_valid(filePath)]
+def main():
+    stdin_contents = ""
+    for line in sys.stdin:
+        stdin_contents += line
 
-print(files_list)
-print("DONE")
+    whitespace_regex_pattern = re.compile(r'\s+')
+    files_list = [filePath for filePath in re.sub(whitespace_regex_pattern, '', stdin_contents).split(',') if path_validation.is_pathname_valid(filePath)]
+
+    checksums_dict = {}
+    for i in range(0, len(files_list)):
+        current_file_path = files_list[i]
+        current_file_md5sum = md5sum(current_file_path)
+        checksums_dict.update({current_file_path:current_file_md5sum})
+
+    # Print results to stdout
+    print(json.dumps(checksums_dict))
+
+if __name__ == "__main__":
+    main()
