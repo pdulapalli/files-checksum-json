f927d5d513e3797128a3a10002f16690f026e703
Anurag Dulapalli <pdulapalli@gmail.com>
2019-06-24 22:22:15 -0400

    Use SHA-256 hash; explicitly set null for nonexistent files

--- files_checksum_json.py
+++ files_checksum_json.py
@@ -7,17 +7,23 @@ import sys
 import re
 import path_validation
 
-def md5sum(filename):
-    hash = hashlib.md5()
-    with open(filename, "rb") as f:
-        for chunk in iter(lambda: f.read(128 * hash.block_size), b""):
-            hash.update(chunk)
+DEFAULT_BLOCK_SIZE = 65536 # 64kb chunks
+
+def sha256sum(fname, blocksize):
+    hash = hashlib.sha256()
+    with open(fname, 'rb') as f:
+        while True:
+            block = f.read(blocksize)
+            if not block:
+                break
+            hash.update(block)
+
     return hash.hexdigest()
 
 def main():
     stdin_contents = ""
     for line in sys.stdin:
-        stdin_contents += line
+        stdin_contents += str(line)
 
     whitespace_regex_pattern = re.compile(r'\s+')
     files_list = [filePath for filePath in re.sub(whitespace_regex_pattern, '', stdin_contents).split(',') if path_validation.is_pathname_valid(filePath)]
@@ -25,8 +31,12 @@ def main():
     checksums_dict = {}
     for i in range(0, len(files_list)):
         current_file_path = files_list[i]
-        current_file_md5sum = md5sum(current_file_path)
-        checksums_dict.update({current_file_path:current_file_md5sum})
+
+        if os.path.exists(current_file_path):
+            current_file_sha256sum = sha256sum(current_file_path, DEFAULT_BLOCK_SIZE)
+            checksums_dict[current_file_path] = current_file_sha256sum
+        else:
+            checksums_dict[current_file_path] = None
 
     # Print results to stdout
     print(json.dumps(checksums_dict))
